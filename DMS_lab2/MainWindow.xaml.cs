e547d2d8ec2dfce2f22bbe7e3ac082388a00be4e
alextkachuk3 <tkachuk.alexey@unicyb.kiev.ua>
2022-11-23 07:26:02 +0200

    Video player UI design

--- DMS_lab2/MainWindow.xaml.cs
+++ DMS_lab2/MainWindow.xaml.cs
@@ -14,7 +14,7 @@ namespace DMS_lab2
             var currentDirectory = new FileInfo(currentAssembly.Location).DirectoryName;
             var libDirectory = new DirectoryInfo(System.IO.Path.Combine(currentDirectory, "vlc"));
             VlcControl.SourceProvider.CreatePlayer(libDirectory);
-            VlcControl.SourceProvider.MediaPlayer.Play(new Uri(""));
+            VlcControl.SourceProvider.MediaPlayer.Play(new Uri("C:\\Users\\olex1\\OneDrive\\Desktop\\DMS\\Result H264.mp4"));
         }
     }
 }
