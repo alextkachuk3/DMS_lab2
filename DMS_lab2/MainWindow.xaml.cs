407979989620981fcc8b5351a935ef7bee7f3389
alextkachuk3 <tkachuk.alexey@unicyb.kiev.ua>
2022-11-23 08:26:37 +0200

    Pause and play video

--- DMS_lab2/MainWindow.xaml.cs
+++ DMS_lab2/MainWindow.xaml.cs
@@ -1,15 +1,13 @@
 ﻿using Microsoft.Win32;
-using System;
 using System.IO;
 using System.Reflection;
 using System.Windows;
-using Vlc.DotNet.Core;
 
 namespace DMS_lab2
 {
     public partial class MainWindow : Window
     {
-        private VlcMediaPlayer player;
+        Player player;
         public MainWindow()
         {
             InitializeComponent();
@@ -17,7 +15,7 @@ namespace DMS_lab2
             var currentDirectory = new FileInfo(currentAssembly.Location).DirectoryName;
             var libDirectory = new DirectoryInfo(Path.Combine(currentDirectory, "vlc"));
             VlcControl.SourceProvider.CreatePlayer(libDirectory);
-            player = VlcControl.SourceProvider.MediaPlayer;
+            player = new Player(VlcControl.SourceProvider.MediaPlayer);
         }
 
         private void openFileButton_Click(object sender, RoutedEventArgs e)
@@ -26,14 +24,29 @@ namespace DMS_lab2
             openFileDialog.Filter = "All Media Files|*.wav;*.aac;*.wma;*.wmv;*.avi;*.mpg;*.mpeg;*.m1v;*.mp2;*.mp3;*.mpa;*.mpe;*.m3u;*.mp4;*.mov;*.3g2;*.3gp2;*.3gp;*.3gpp;*.m4a;*.cda;*.aif;*.aifc;*.aiff;*.rmi;*.mkv;*.WAV;*.AAC;*.WMA;*.WMV;*.AVI;*.MPG;*.MPEG;*.M1V;*.MP2;*.MP3;*.MPA;*.MPE;*.M3U;*.MP4;*.MOV;*.3G2;*.3GP2;*.3GP;*.3GPP;*.M4A;*.CDA;*.AIF;*.AIFC;*.AIFF;*.MID;*.MIDI;*.RMI;*.MKV";
             if (openFileDialog.ShowDialog() is true)
             {
-                player.Play(new Uri(openFileDialog.FileName));
+                player.Play(openFileDialog.FileName);
+                playButtonIcon.Kind = MaterialDesignThemes.Wpf.PackIconKind.Pause;
                 UpdateFileName();
             }
         }
 
         private void UpdateFileName()
         {
-            fileNameTextBlock.Text = VlcControl.SourceProvider.MediaPlayer.GetMedia().Title;
+            fileNameTextBlock.Text = player.GetMediaTitle();
+        }
+
+        private void playButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(player.IsPaused() is true)
+            {                
+                player.Continue();
+                playButtonIcon.Kind = MaterialDesignThemes.Wpf.PackIconKind.Pause;
+            }
+            else
+            {
+                player.Pause();
+                playButtonIcon.Kind = MaterialDesignThemes.Wpf.PackIconKind.Play;
+            }
         }
     }
 }
---
+++ DMS_lab2/Player.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vlc.DotNet.Core;
+using Vlc.DotNet.Wpf;
+
+namespace DMS_lab2
+{
+    internal class Player
+    {
+        private VlcMediaPlayer player;
+        private bool paused;
+        public Player(VlcMediaPlayer player)
+        {
+            this.player = player;
+            paused = false;
+        }
+
+        public string GetMediaTitle()
+        {
+            return player.GetMedia().Title;
+        }
+
+        public void Play(string URI)
+        {
+            player.Play(new Uri(URI));
+            paused = false;
+        }
+
+        public void Continue()
+        {
+            if (paused is true)
+            {
+                paused = false;
+                player.Play();
+            }
+        }
+
+        public void Pause()
+        {
+            if (paused is false)
+            {
+                paused = true;
+                player.Pause();
+            }            
+        }
+
+        public bool IsPaused()
+        {
+            return paused;
+        }
+    }
+}
