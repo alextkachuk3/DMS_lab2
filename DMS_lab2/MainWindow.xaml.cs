b3d4a76343658aafbeb188379e32274402a55e7e
alextkachuk3 <tkachuk.alexey@unicyb.kiev.ua>
2022-11-23 07:45:20 +0200

    Ability to select a local file to play

--- DMS_lab2/MainWindow.xaml.cs
+++ DMS_lab2/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.IO;
 using System.Reflection;
 using System.Windows;
@@ -12,9 +13,18 @@ namespace DMS_lab2
             InitializeComponent();
             var currentAssembly = Assembly.GetEntryAssembly();
             var currentDirectory = new FileInfo(currentAssembly.Location).DirectoryName;
-            var libDirectory = new DirectoryInfo(System.IO.Path.Combine(currentDirectory, "vlc"));
-            VlcControl.SourceProvider.CreatePlayer(libDirectory);
-            VlcControl.SourceProvider.MediaPlayer.Play(new Uri("C:\\Users\\olex1\\OneDrive\\Desktop\\DMS\\Result H264.mp4"));
+            var libDirectory = new DirectoryInfo(Path.Combine(currentDirectory, "vlc"));
+            VlcControl.SourceProvider.CreatePlayer(libDirectory);           
+        }
+
+        private void openFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "All Media Files|*.wav;*.aac;*.wma;*.wmv;*.avi;*.mpg;*.mpeg;*.m1v;*.mp2;*.mp3;*.mpa;*.mpe;*.m3u;*.mp4;*.mov;*.3g2;*.3gp2;*.3gp;*.3gpp;*.m4a;*.cda;*.aif;*.aifc;*.aiff;*.rmi;*.mkv;*.WAV;*.AAC;*.WMA;*.WMV;*.AVI;*.MPG;*.MPEG;*.M1V;*.MP2;*.MP3;*.MPA;*.MPE;*.M3U;*.MP4;*.MOV;*.3G2;*.3GP2;*.3GP;*.3GPP;*.M4A;*.CDA;*.AIF;*.AIFC;*.AIFF;*.MID;*.MIDI;*.RMI;*.MKV";
+            if(openFileDialog.ShowDialog() is true)
+            {
+                VlcControl.SourceProvider.MediaPlayer.Play(new Uri(openFileDialog.FileName));
+            }
         }
     }
 }
